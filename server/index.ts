f54d897b4e26555d5580bd36794379792e44060d
pranav-suri <pranavsuri.dev@projecthumancity.com>
2024-10-23 01:57:45 +0530

    docker setup

--- server/index.ts
+++ server/index.ts
@@ -2,20 +2,20 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello World! The time is " + new Date());
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 console.log("Hello via Bun!");
 setInterval(() => {
     console.log("Hello World! The time is " + new Date());
-    console.error("Error message: ", new Date())
+    console.error("Error message: ", new Date());
 }, 5000);
